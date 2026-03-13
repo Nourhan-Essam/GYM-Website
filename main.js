7218bfb69120564a2e4f28f90a120df445b8753d
Nourhan-Essam <nourhanessam102@gmail.com>
2026-03-13 21:34:05 +0200

    fix the buttons, add about us section and guides section

---
+++ main.js
@@ -0,0 +1,12 @@
+function GoToSupplements(){
+    window.location.href = "supplements.html";
+}
+function GoToEquipments(){
+    window.location.href = "equipments.html";
+}
+function GoToAppareal(){
+    window.location.href = "appareal.html";
+}
+function GoToAccessories(){
+    window.location.href = "accessories.html";
+}
